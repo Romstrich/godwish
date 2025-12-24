4d281998da7575350f3af2dca8c07a5af0929867
Romstrich <roman.motrich@yandex.ru>
2025-12-24 09:54:16 +0300

    загружаем несколько картинок

--- godwish/elements/models.py
+++ godwish/elements/models.py
@@ -11,9 +11,9 @@ from django.db import models
 
 class Picture(models.Model):
     id=models.IntegerField(primary_key=True)
-    name=models.CharField(verbose_name="Название изображения",max_length=128,null=True)
-    comment=models.CharField(verbose_name="Комментарий к изображению", max_length=256,null=True)
-    picture=models.ImageField(verbose_name="Изображение",null=False,upload_to='galery/',blank=True)
+    name=models.CharField(verbose_name="Название изображения",max_length=128,blank=True)
+    comment=models.CharField(verbose_name="Комментарий к изображению", max_length=256,blank=True)
+    picture=models.ImageField(verbose_name="Изображение",upload_to='galery/',blank=True, null=True)
     update=models.DateTimeField(verbose_name="Время загрузки",auto_now_add=True)
     #Включить автора загрузки
     #Включить "хозяина" Картинки
--- godwish/elements/views.py
+++ godwish/elements/views.py
@@ -21,11 +21,30 @@ class PictureWorkView(View):
         content={'form':form}
         return render(request,context=content,template_name='picture_upload.html')
     def post(self,request):
-        form=UpPicture(request.POST,request.FILES)
-        if form.is_valid():
-            #for img in request.FILES.getlist('images')
-            form.save()
-            img_obj=form.instance
+        # form=UpPicture(request.POST,request.FILES)
+        # if form.is_valid():
+        #     #for img in request.FILES.getlist('images')
+        #     form.save()
+        #     img_obj=form.instance
+        #Получим картинки
+        uploaded_images = request.FILES.getlist('picture')
+        #'_post': <QueryDict: {'csrfmiddlewaretoken': ['JQkGZHdbEzr0OSFybtm4k6fJ4bTp6Ycb4drfVc4Rip4Eamz63cDZZifiNdeW3prO'], 'name': ['sdfasdf'], 'comment': ['asdfasdf'], 'picture': ['']}>, '_files': <MultiValueDict: {}>
+        #Получим комментарий и имя
+        commentPic=request.POST['comment']
+        namePic=comment=request.POST['name']
+        upImageLen=len(uploaded_images)
+        for image in uploaded_images:
+            print(image._name)
+            #Сохраним картинку
+            #Если картинка одна и есть name из формы:
+            if upImageLen == 1:
+                if namePic:
+                    Picture.objects.create(name=namePic, comment=commentPic, picture=image)
+                else:
+                    Picture.objects.create(name=image._name, comment=commentPic, picture=image)
+            else:
+                Picture.objects.create(name=image._name,comment=commentPic,picture=image)
+
         return render(request,template_name='picture_upload.html')
 
 class GaleryView(View):
